e40971cef378e8d7118768a33c1867782caa5b53
Leo Liu <11664880+silver886@users.noreply.github.com>
2024-10-30 13:49:42 -0700

    Implement dynamic loading

--- scripts/common.js
+++ scripts/common.js
@@ -1,4 +1,19 @@
-(() => {
-    $('nav#placeholder').load('./text/nav_after_login.html');
-    $('footer#placeholder').load('./text/footer.html');
-})();
+const fetchComponent = async (fileName) => {
+    try {
+        const response = await fetch(
+            `/components/${fileName}.html`,
+        );
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        return await response.text();
+    } catch (error) {
+        console.error(error.message);
+        alert('Failed to fetch component');
+    }
+}
+
+window.onload = async () => {
+    document.querySelector('footer.placeholder').innerHTML = await fetchComponent('bottomNav');
+}
