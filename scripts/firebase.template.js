e16dda458af153cde76d0b652a60517f999e2403
Leo Liu <11664880+silver886@users.noreply.github.com>
2024-10-30 18:52:25 -0700

    Deploy to Firebase

--- scripts/firebase.template.js
+++
@@ -1,8 +0,0 @@
-firebase.initializeApp({
-    apiKey: "KjUoDg3Uy1mWSnxuoKhvQL7sqweyJnTMD1VuaRM",
-    authDomain: "comp1800-202430-9bcfa.firebaseapp.com",
-    projectId: "comp1800-202430-9bcfa",
-    storageBucket: "comp1800-202430-9bcfa.appspot.com",
-    messagingSenderId: "818539153471",
-    appId: "1:818539153471:web:b8922487ef951d7ca1158d"
-});
