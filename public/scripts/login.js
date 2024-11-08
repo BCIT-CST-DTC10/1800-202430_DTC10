7a59bce3bb7f8ec50a25bbdbd8cf9b05eaba5438
Leo Liu <11664880+silver886@users.noreply.github.com>
2024-11-08 15:25:56 -0800

    Setup user database

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,4 +1,6 @@
-(new firebaseui.auth.AuthUI(firebase.auth())).start("#firebaseui-auth-container", {
+const auth = firebase.auth();
+
+(new firebaseui.auth.AuthUI(auth)).start("#firebaseui-auth-container", {
     signInFlow: "popup",
     signInSuccessUrl: "/",
     signInOptions: [
@@ -7,4 +9,25 @@
     ],
     tosUrl: "/term",
     privacyPolicyUrl: "/policy",
+});
+
+const db = firebase.firestore();
+
+auth.onAuthStateChanged(async (user) => {
+    if (user) {
+        const firebaseUser = db.collection("users").doc(user.uid);
+
+        try {
+            if (!(await firebaseUser.get()).exists) {
+                throw new Error("Empty user");
+            }
+        } catch (err) {
+            await firebaseUser.set({
+                createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+                updatedAt: firebase.firestore.FieldValue.serverTimestamp(),
+            });
+        }
+
+        window.location = "/";
+    }
 });
