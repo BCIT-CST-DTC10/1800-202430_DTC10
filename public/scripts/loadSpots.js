e34828acda7cffd868b805c3792b0e584c38a396
Leo Liu <11664880+silver886@users.noreply.github.com>
2024-11-07 23:00:20 -0800

    Refactor loadSpots

--- public/scripts/loadSpots.js
+++ public/scripts/loadSpots.js
@@ -1,43 +1,107 @@
 (async () => {
-    const firestore = firebase.firestore();
-    const storage = firebase.storage();
+    const [spotCard, star, starHalf, starOutline, firestoreLocations, firestoreRates, firestoreStudySpotsData, storageImages] = await Promise.all([
+        fetchComponent("cards/spot"),
+        fetchIcon("star"),
+        fetchIcon("starHalf"),
+        fetchIcon("starOutline"),
+        firebase.firestore().collection("tags").where("type", "==", "location").get(),
+        firebase.firestore().collection("reviews").get(),
+        firebase.firestore().collection("studySpots").get(),
+        (async () => {
+            return Object.fromEntries(await Promise.all((await firebase.storage().ref().listAll()).items.map((v) => {
+                return new Promise((resolve, reject) => {
+                    v.getDownloadURL().then((w) => {
+                        resolve([v.fullPath, w]);
+                    }).catch((err) => {
+                        reject(err);
+                    });
+                })
 
-    const studySpots = firestore.collection("studySpots");
+            })));
+        })(),
+    ])
 
-    const spotCard = await fetchComponent('cards/spot');
-    const star = await fetchIcon('star');
-    const starHalf = await fetchIcon('starHalf');
-    const starOutline = await fetchIcon('starOutline');
+    const locations = {};
+    firestoreLocations.forEach((v) => {
+        locations[v.id] = v.data().name;
+    });
 
-    const studySpotsData = await studySpots.get();
-    if (studySpotsData.empty) {
-        throw new Error("Empty studySpots");
-    }
-    studySpotsData.forEach(async (v) => {
-        const data = v.data()
+    const rates = {};
+    firestoreRates.forEach((v) => {
+        const data = v.data();
+        if (!rates.hasOwnProperty(data.studySpotId)) {
+            rates[data.studySpotId] = {
+                count: 0,
+                rate: 0,
+            };
+        }
+        rates[data.studySpotId].count++;
+        rates[data.studySpotId].rate += data.rating;
+    });
+    Object.entries(rates).forEach(([k, v]) => {
+        rates[k] = v.rate / v.count;
+    })
 
-        const image = await storage.ref(v.data().images[0]).getDownloadURL()
+    const studySpotList = [];
+    firestoreStudySpotsData.forEach((v) => {
+        const data = v.data();
 
-        document.querySelector('div.toBeReplaced#spotList').innerHTML += spotCard.
-            replaceAll('{{id}}', v.id).
-            replaceAll('{{image}}', image).
-            replaceAll('{{name}}', data.name).
-            replaceAll('{{desc}}', data.description).
-            replaceAll('{{type}}', data.type).
-            replaceAll('{{star}}', generateRateStar(data.rate).reduce((pre, cur, i) => {
+        studySpotList.push({
+            id: v.id,
+            image: v.data().images[0],
+            name: data.name,
+            desc: data.description,
+            type: Object.entries(data.tags).filter(([k, v]) => {
+                return locations.hasOwnProperty(k) && v.status
+            }).map((v) => {
+                return locations[v[0]]
+            }).reduce((p, c) => {
+                if (c) {
+                    return `${p} ${c}`
+                } else {
+                    return p
+                }
+            }, ""),
+            rate: rates[v.id] ?? 0,
+        });
+    });
+    studySpotList.sort((i, j) => {
+        if (i.rate > j.rate) {
+            return -1;
+        } else if (j.rate > i.rate) {
+            return 1;
+        }
+
+        if (i.name > j.name) {
+            return 1;
+        } else if (j.name > i.name) {
+            return -1;
+        }
+
+        return 0;
+    });
+
+    document.querySelector("div.toBeReplaced#spotList").innerHTML = studySpotList.reduce((p, c) => {
+        return p + spotCard.
+            replaceAll("{{id}}", c.id).
+            replaceAll("{{image}}", storageImages[c.image]).
+            replaceAll("{{name}}", c.name).
+            replaceAll("{{desc}}", c.desc).
+            replaceAll("{{type}}", c.type).
+            replaceAll("{{star}}", generateRateStar(c.rate).reduce((p, c, i) => {
                 switch (i) {
                     case 0:
-                        return pre + star.repeat(cur);
+                        return p + star.repeat(c);
                     case 1:
-                        return pre + starHalf.repeat(cur);
+                        return p + starHalf.repeat(c);
                     case 2:
-                        return pre + starOutline.repeat(cur);
+                        return p + starOutline.repeat(c);
                 }
-            }, '')).
-            replaceAll('{{rate}}', v.rate);
+            }, "")).
+            replaceAll("{{rate}}", c.rate);
+    }, document.querySelector("div.toBeReplaced#spotList").innerHTML)
 
-        document.querySelectorAll('div.toBeReplaced#spotList svg').forEach((v) => {
-            v.style = 'display: inline-block; margin: auto 0; fill: #000;'
-        })
+    document.querySelectorAll("div.toBeReplaced#spotList svg").forEach((w) => {
+        w.style = "display: inline-block; margin: auto 0; fill: #000;"
     })
 })();
