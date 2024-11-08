65613a384a1d8c2cc98dff3b1becc2970bec65b8
Leo Liu <11664880+silver886@users.noreply.github.com>
2024-11-08 14:06:26 -0800

    Remove unused part

--- public/scripts/common.js
+++ public/scripts/common.js
@@ -51,5 +51,4 @@ const loadRateStar = async (rate) => {
 
 (async () => {
     document.querySelector("nav.toBeReplaced").innerHTML = await fetchComponent("topNav");
-    document.querySelector("footer.toBeReplaced").innerHTML = await fetchComponent("bottomNav");
 })();
