280ad0f746514461defc37b259b28eb64dba316e
Leo Liu <11664880+silver886@users.noreply.github.com>
2024-11-11 17:31:31 -0800

    Inject data into search

---
+++ public/scripts/search.js
@@ -0,0 +1,72 @@
+(async () => {
+    const [spotCard, star, starHalf, starOutline, types, features, spots] = await Promise.all([
+        fetchComponent("cards/spot"),
+        fetchIcon("star"),
+        fetchIcon("starHalf"),
+        fetchIcon("starOutline"),
+        fetchFirestoreTypes(),
+        fetchFirestoreFeatures(),
+        fetchFirestoreSpots(),
+    ]);
+
+    const spotKeys = Object.keys(spots);
+    const [images, ...reviews] = await Promise.all([
+        fetchStorageFilesBySpotIds(spotKeys),
+        ...spotKeys.map((v) =>
+            fetchFirestoreReviews({
+                spotIds: [v],
+            })),
+    ]);
+    const ratings = Object.fromEntries(spotKeys.map((v, i) => [v, calculateRatingFromReviews(reviews[i])]));
+
+    const aggSpots = Object.entries(spots).
+        map(([k, v]) => ({
+            id: k,
+            image: Object.values(images[k])[0],
+            name: v.name,
+            description: v.description,
+            typeName: types[v.type].name,
+            type: v.type,
+            features: v.features,
+            rating: ratings[k],
+        }));
+
+    document.querySelector("div.toBeReplaced#top-spot-List").innerHTML = aggSpots.
+        sort((i, j) => {
+            if (i.rating > j.rating) {
+                return -1;
+            } else if (j.rating > i.rating) {
+                return 1;
+            }
+
+            if (i.name > j.name) {
+                return 1;
+            } else if (j.name > i.name) {
+                return -1;
+            }
+
+            return 0;
+        }).
+        slice(0, 5).
+        reduce((p, c) => p + spotCard.
+            replaceAll("{{id}}", c.id).
+            replaceAll("{{image}}", c.image).
+            replaceAll("{{name}}", c.name).
+            replaceAll("{{description}}", c.description).
+            replaceAll("{{type}}", c.typeName).
+            replaceAll("{{star}}", generatingRatingStar(c.rating).reduce((p, c, i) => {
+                switch (i) {
+                    case 0:
+                        return p + star.repeat(c);
+                    case 1:
+                        return p + starHalf.repeat(c);
+                    case 2:
+                        return p + starOutline.repeat(c);
+                }
+            }, "")).
+            replaceAll("{{rating}}", c.rating), document.querySelector("div.toBeReplaced#top-spot-List").innerHTML);
+
+    document.querySelectorAll("div.toBeReplaced#top-spot-List svg").forEach((w) => {
+        w.style = "display: inline-block; margin: auto 0; fill: #000;";
+    });
+})();
