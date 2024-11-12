f5a654885666e6d3b623b9fe4d0dea82f86bb0fb
Leo Liu <11664880+silver886@users.noreply.github.com>
2024-11-11 18:17:10 -0800

    Implement filter

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -30,7 +30,40 @@
             type: v.type,
             features: v.features,
             rating: ratings[k],
-        }));
+        })).
+        sort((i, j) => {
+            if (i.rating > j.rating) {
+                return -1;
+            } else if (j.rating > i.rating) {
+                return 1;
+            }
+
+            if (i.name > j.name) {
+                return 1;
+            } else if (j.name > i.name) {
+                return -1;
+            }
+
+            return 0;
+        });
+
+    const callbackReduceAggSpotsToSpotCards = (p, c) => p + spotCard.
+        replaceAll("{{id}}", c.id).
+        replaceAll("{{image}}", c.image).
+        replaceAll("{{name}}", c.name).
+        replaceAll("{{description}}", c.description).
+        replaceAll("{{type}}", c.typeName).
+        replaceAll("{{star}}", generatingRatingStar(c.rating).reduce((p, c, i) => {
+            switch (i) {
+                case 0:
+                    return p + star.repeat(c);
+                case 1:
+                    return p + starHalf.repeat(c);
+                case 2:
+                    return p + starOutline.repeat(c);
+            }
+        }, "")).
+        replaceAll("{{rating}}", c.rating);
 
     document.querySelector("div#filter-section").innerHTML = Object.entries(types).
         map(([k, v]) => ({
@@ -51,41 +84,34 @@
             replaceAll("{{name}}", c.name), document.querySelector("div#filter-section").innerHTML);
 
     document.querySelector("div.toBeReplaced#top-spot-List").innerHTML = aggSpots.
-        sort((i, j) => {
-            if (i.rating > j.rating) {
-                return -1;
-            } else if (j.rating > i.rating) {
-                return 1;
-            }
-
-            if (i.name > j.name) {
-                return 1;
-            } else if (j.name > i.name) {
-                return -1;
-            }
-
-            return 0;
-        }).
         slice(0, 5).
-        reduce((p, c) => p + spotCard.
-            replaceAll("{{id}}", c.id).
-            replaceAll("{{image}}", c.image).
-            replaceAll("{{name}}", c.name).
-            replaceAll("{{description}}", c.description).
-            replaceAll("{{type}}", c.typeName).
-            replaceAll("{{star}}", generatingRatingStar(c.rating).reduce((p, c, i) => {
-                switch (i) {
-                    case 0:
-                        return p + star.repeat(c);
-                    case 1:
-                        return p + starHalf.repeat(c);
-                    case 2:
-                        return p + starOutline.repeat(c);
-                }
-            }, "")).
-            replaceAll("{{rating}}", c.rating), document.querySelector("div.toBeReplaced#top-spot-List").innerHTML);
+        reduce(callbackReduceAggSpotsToSpotCards, document.querySelector("div.toBeReplaced#top-spot-List").innerHTML);
 
     document.querySelectorAll("div.toBeReplaced#top-spot-List svg").forEach((w) => {
         w.style = "display: inline-block; margin: auto 0; fill: #000;";
     });
+
+    document.querySelectorAll("div#filter-section>a.button").forEach((v) => {
+        v.addEventListener("click", (e) => {
+            if (e.target.classList.contains("activated")) {
+                e.target.classList.remove("activated");
+            } else {
+                e.target.classList.add("activated");
+            }
+        });
+        v.addEventListener("click", (e) => {
+            const types = []
+            document.querySelectorAll("div#filter-section>a.button.activated").forEach((v) => {
+                types.push(v.id);
+            });
+            if (types.length) {
+                document.querySelector("div.toBeReplaced#top-spot-List").innerHTML = aggSpots.
+                    filter((v) => types.includes(v.type)).
+                    reduce(callbackReduceAggSpotsToSpotCards, "");
+            } else {
+                document.querySelector("div.toBeReplaced#top-spot-List").innerHTML = aggSpots.
+                    reduce(callbackReduceAggSpotsToSpotCards, "");
+            }
+        });
+    })
 })();
