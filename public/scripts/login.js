18f556d615b79156b82bd6e68e5678fda9544174
Leo Liu <11664880+silver886@users.noreply.github.com>
2024-11-11 16:50:02 -0800

    Improve login experience

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,23 +1,21 @@
-(new firebaseui.auth.AuthUI(firebase.auth())).start("#firebaseui-auth-container", {
-    signInFlow: "popup",
-    signInSuccessUrl: "/",
-    signInOptions: [
-        firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    ],
-    tosUrl: "/term",
-    privacyPolicyUrl: "/policy",
-    callbacks: {
-        // uiShown: () => {
-        //     // The widget is rendered.
-        //     // Hide the loader.
-        //     document.getElementById('loader').style.display = 'none';
-        // },
-    }
-});
-
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
         window.location = "/";
+    } else {
+        (new firebaseui.auth.AuthUI(firebase.auth())).start("#firebaseui-auth-container", {
+            signInFlow: "popup",
+            signInSuccessUrl: "/",
+            signInOptions: [
+                firebase.auth.EmailAuthProvider.PROVIDER_ID,
+                firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            ],
+            tosUrl: "/term",
+            privacyPolicyUrl: "/policy",
+            callbacks: {
+                uiShown: () => {
+                    document.getElementById('message').style.display = 'none';
+                },
+            }
+        });
     }
 });
