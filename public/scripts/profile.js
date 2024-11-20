12035229db3ce6cadfbc7d03d6d6548b39591c96
chaara-dev <chaara.dev@gmail.com>
2024-11-20 10:21:05 -0800

    Implement review title and stars in recents

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -21,6 +21,22 @@
 
     document.querySelectorAll("main>div.reviewContainer>div.review.toBeReplaced")
         .forEach((v, i) => {
+            console.log(`Title: ${reviews[i].title.length}`)
+            console.log(`Comment: ${reviews[i].comment.length}`)
+            if (reviews[i].title == "") {
+                reviews[i].title = "No title found"
+            }
+            else if (reviews[i].comment == "") {
+                reviews[i].comment = "No comment found"
+            }
+            if (reviews[i].title.length > 40) {
+                reviews[i].title = reviews[i].title.slice(0, 40)
+                console.log("title smaller")
+            }
+            if (reviews[i].comment.length > 100) {
+                reviews[i].comment = reviews[i].comment.slice(0, 100)
+                console.log("comment smaller")
+            }
             v.innerHTML = `
             <span class="reviewText">
             <span class="reviewTitle">
