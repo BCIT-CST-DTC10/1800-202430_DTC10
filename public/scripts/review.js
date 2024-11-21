1e11a8f56578efffe1d89bab77d8d456a799cf35
Kiara Cherry <kiaracherry2006@gmail.com>
2024-11-21 10:33:27 -0800

    Add alert for when new review is added

--- public/scripts/review.js
+++ public/scripts/review.js
@@ -75,6 +75,7 @@
             createdAt: firebase.firestore.FieldValue.serverTimestamp(),
             updatedAt: firebase.firestore.FieldValue.serverTimestamp(),
         });
+        alert("New review created")
         window.location.reload();
     });
 
