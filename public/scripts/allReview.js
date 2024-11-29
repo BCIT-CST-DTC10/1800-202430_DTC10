d68070d25b68c026a2f724bbcf92bfbf91acd91b
Leo Liu <11664880+silver886@users.noreply.github.com>
2024-11-29 11:45:57 -0800

    Fix delete bottom on all review page

--- public/scripts/allReview.js
+++ public/scripts/allReview.js
@@ -1,5 +1,12 @@
 (async () => {
-    const [user, spots] = await Promise.all([fetchFirebaseUser(), fetchFirestoreSpots()])
+    const [reviewCard, star, starHalf, starOutline, user, spots] = await Promise.all([
+        fetchComponent("cards/review"),
+        fetchIcon("star"),
+        fetchIcon("starHalf"),
+        fetchIcon("starOutline"),
+        fetchFirebaseUser(),
+        fetchFirestoreSpots(),
+    ])
 
     const invalidReviews = []
     const reviews = Object.entries(await fetchFirestoreReviews({ userIds: [user.uid] }))
@@ -24,83 +31,43 @@
 
     document.querySelector("span.username").innerHTML = user.displayName;
 
-    const reviewContainer = document.querySelector("main>div.reviewContainer");
-
-    reviewContainer.innerHTML = ``;
-
-    const generateRatingStars = (rating) => {
-        const star = "<img src='/icons/star.svg' class='star-icon'>";
-        const halfStar = "<img src='/icons/starHalf.svg' class='star-icon'>";
-        const emptyStar = "<img src='/icons/starOutline.svg' class='star-icon'>";
-        let stars = "";
-
-        const fullStars = Math.floor(rating);
-        const halfStars = (rating % 1 >= 0.5) ? 1 : 0;
-        const emptyStars = 5 - fullStars - halfStars;
-
-        stars += star.repeat(fullStars);
-        stars += halfStar.repeat(halfStars);
-        stars += emptyStar.repeat(emptyStars);
-
-        return stars;
-    };
-
-    reviews.forEach((review) => {
-        const reviewElement = document.createElement(`div`);
-        reviewElement.classList.add(`review`);
-
-        reviewElement.innerHTML = `
-        <a href="/detail?id=${review.spotId}" class="links">
-            <div class="review-header">
-                <div class="review-rating">${review.spotName}</div>
-                <div class="review-rating">${generateRatingStars(review.rating)}</div>
-                <h3 class="review-title">${review.title ? review.title : "No title"}</h3>
-                <h5 class="review-time">${review.createdAt.toDate().toLocaleString("en-CA")}</h5>
-            </div>
-            <div class="review-body">
-                <p class="review-comment">${review.comment ? review.comment : "No comment"}</p>
-                <img src="/icons/delete.svg" class="delete-button"
-                style="float: right; cursor: pointer; border: 2px solid #EA3323; border-radius: 5px; position: absolute; right: 16px; bottom: 35px;"
-                id="{{id}}" userId="{{userId}}">
-            </div>
-        </a>
-        `;
-
-        reviewContainer.appendChild(reviewElement)
-    });
+    document.querySelector("div.reviewContainer").innerHTML = Object.values(reviews)
+        .sort((a, b) => b.createdAt - a.createdAt)
+        .reduce((p, c) => p + reviewCard
+            .replaceAll("{{id}}", c.id)
+            .replaceAll("{{userId}}", c.userId)
+            .replaceAll("{{user}}", c.spotName)
+            .replaceAll("{{title}}", c.title)
+            .replaceAll("{{createdAt}}", c.createdAt.toDate().toLocaleString("en-CA"))
+            .replaceAll("{{comment}}", c.comment)
+            .replaceAll("{{star}}", generatingRatingStar(c.rating).reduce((p, c, i) => {
+                switch (i) {
+                    case 0:
+                        return p + star.trim().repeat(c);
+                    case 1:
+                        return p + starHalf.trim().repeat(c);
+                    case 2:
+                        return p + starOutline.trim().repeat(c);
+                }
+            }, "")), document.querySelector("div.reviewContainer").innerHTML);
 
     document.querySelectorAll("img.delete-button").forEach((v) => {
         v.addEventListener("click", async (e) => {
-            if (user.uid === e.target.attributes.userId.value) {
-                if ((await Sweetalert2.fire({
-                    title: "Delete review",
-                    text: "Are you sure you want to delete your review?",
-                    icon: "question",
-                    showCancelButton: true,
-                    focusCancel: true,
-                })).isConfirmed) {
-                    await firebase.firestore().collection("reviews").doc(e.target.attributes.id.value).delete();
-                    await Sweetalert2.fire({
-                        title: "Success",
-                        text: "Review deleted",
-                        icon: "success",
-                    });
-                    window.location.reload();
-                }
+            if ((await Sweetalert2.fire({
+                title: "Delete review",
+                text: "Are you sure you want to delete your review?",
+                icon: "question",
+                showCancelButton: true,
+                focusCancel: true,
+            })).isConfirmed) {
+                await firebase.firestore().collection("reviews").doc(e.target.id).delete();
+                await Sweetalert2.fire({
+                    title: "Success",
+                    text: "Review deleted",
+                    icon: "success",
+                });
+                window.location.reload();
             }
         });
     });
-
-    if (user) {
-        document.querySelectorAll("img.delete-button").forEach((v) => {
-            if (user.uid !== v.attributes.userId.value) {
-                v.style.display = "none";
-            }
-        });
-    } else {
-        document.querySelector("main>section>div").style.display = "none";
-        document.querySelectorAll("img.delete-button").forEach((v) => {
-            v.style.display = "none";
-        });
-    }
 })();
