0489c7549d0c7861d00fda4ec074d0f32a1ad715
Leo Liu <11664880+silver886@users.noreply.github.com>
2024-11-29 11:54:38 -0800

    Link review card to detail page

--- public/scripts/allReview.js
+++ public/scripts/allReview.js
@@ -33,26 +33,33 @@
 
     document.querySelector("div.reviewContainer").innerHTML = Object.values(reviews)
         .sort((a, b) => b.createdAt - a.createdAt)
-        .reduce((p, c) => p + reviewCard
-            .replaceAll("{{id}}", c.id)
-            .replaceAll("{{userId}}", c.userId)
-            .replaceAll("{{user}}", c.spotName)
-            .replaceAll("{{title}}", c.title)
-            .replaceAll("{{createdAt}}", c.createdAt.toDate().toLocaleString("en-CA"))
-            .replaceAll("{{comment}}", c.comment)
-            .replaceAll("{{star}}", generatingRatingStar(c.rating).reduce((p, c, i) => {
-                switch (i) {
-                    case 0:
-                        return p + star.trim().repeat(c);
-                    case 1:
-                        return p + starHalf.trim().repeat(c);
-                    case 2:
-                        return p + starOutline.trim().repeat(c);
-                }
-            }, "")), document.querySelector("div.reviewContainer").innerHTML);
+        .reduce((p, c) => p + (() => {
+            const a = document.createElement("a");
+            a.href = `/detail?id=${c.spotId}`;
+            a.style.textDecoration = "none";
+            a.innerHTML = reviewCard
+                .replaceAll("{{id}}", c.id)
+                .replaceAll("{{userId}}", c.userId)
+                .replaceAll("{{user}}", c.spotName)
+                .replaceAll("{{title}}", c.title)
+                .replaceAll("{{createdAt}}", c.createdAt.toDate().toLocaleString("en-CA"))
+                .replaceAll("{{comment}}", c.comment)
+                .replaceAll("{{star}}", generatingRatingStar(c.rating).reduce((p, c, i) => {
+                    switch (i) {
+                        case 0:
+                            return p + star.trim().repeat(c);
+                        case 1:
+                            return p + starHalf.trim().repeat(c);
+                        case 2:
+                            return p + starOutline.trim().repeat(c);
+                    }
+                }, ""));
+            return a.outerHTML;
+        })(), document.querySelector("div.reviewContainer").innerHTML);
 
     document.querySelectorAll("img.delete-button").forEach((v) => {
         v.addEventListener("click", async (e) => {
+            e.preventDefault();
             if ((await Sweetalert2.fire({
                 title: "Delete review",
                 text: "Are you sure you want to delete your review?",
